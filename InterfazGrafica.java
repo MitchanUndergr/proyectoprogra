738ec5cb84be797464aefcf360daae1c5fa12646
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-06-14 19:59:49 -0400

    se agrega nueva clase

--- InterfazGrafica.java
+++ InterfazGrafica.java
@@ -9,7 +9,7 @@ class InterfazGrafica extends JFrame {
     private JPanel panel1;
     public InterfazGrafica() {
         // Configuración de la ventana
-        setTitle("Mi Aplicación");
+        setTitle("Autobuses C&N");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(null); // Establecer null layout
 
@@ -30,7 +30,7 @@ class InterfazGrafica extends JFrame {
         panel1.setBounds(850, 398, 200, 30);
         panel1.setOpaque(false);
 
-        String[] textos = {"¿De donde partes?", "¿A donde quieres ir?", "Ida", "Vuelta"};
+        String[] textos = {"¿De donde partes?", "¿A donde quieres ir?", "Día", "Hora"};
 
         // Crear las barras desplegables y establecer sus coordenadas
         for (int i = 0; i < 4; i++) {
@@ -43,10 +43,11 @@ class InterfazGrafica extends JFrame {
             comboBox.setBounds(x, y, 200, comboBox.getPreferredSize().height); // Establecer posición y tamaño
 
             JLabel label = new JLabel(textos[i]);
-            label.setBounds(x, y - 20, 200, 20); // Establecer la posición y el tamaño del JLabel
-            label.setForeground(Color.DARK_GRAY); // Color de fuente más oscuro
+            label.setBounds(x, y - 22, 200, 20); // Establecer la posición y el tamaño del JLabel
+            //label.setForeground(Color.DARK_GRAY); // Color de fuente más oscuro
+            label.setForeground(new Color(0, 0, 0));
             Font font = label.getFont();
-            Font fontNueva = font.deriveFont(font.getSize() + 3.0f); // Aumentar en 2 puntos el tamaño de la fuente
+            Font fontNueva = font.deriveFont(font.getSize() + 4.0f); // Aumentar en 2 puntos el tamaño de la fuente
             label.setFont(fontNueva);
             add(label);
             add(comboBox); // Agregar al contenedor principal
@@ -70,8 +71,8 @@ class InterfazGrafica extends JFrame {
                 dispose();
 
                 // Abrir nueva ventana
-                NuevaVentana nuevaVentana = new NuevaVentana();
-                nuevaVentana.setVisible(true);
+                Pasajes nuevopasaje = new Pasajes();
+                nuevopasaje.setVisible(true);
             }
         });
 
@@ -79,9 +80,10 @@ class InterfazGrafica extends JFrame {
 
 }
 
-class NuevaVentana extends JFrame {
+class Pasajes extends JFrame {
     private Image imagenfondo1;
-    public NuevaVentana() {
+    private JPanel panel1;
+    public Pasajes() {
         // Configuración de la ventana
         setTitle("Mi compra");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -100,7 +102,68 @@ class NuevaVentana extends JFrame {
         JLabel principal = new JLabel(new ImageIcon(imagenfondo1));
         setContentPane(principal);
 
+        panel1 = new JPanel();
+        panel1.setBounds(850, 398, 200, 30);
+        panel1.setOpaque(false);
+
+        JLabel label = new JLabel("Pasajes");
+        label.setBounds(110, 165, 200, 20); // Establecer la posición y el tamaño del JLabel
+        label.setForeground(new Color(35, 35, 35)); // Color de fuente más oscuro
+        Font font = label.getFont();
+        Font fontNueva = font.deriveFont(font.getSize() + 4.0f); // Aumentar en 2 puntos el tamaño de la fuente
+        label.setFont(fontNueva);
+        add(label);
+
+        JButton boton = new JButton("Buscar");
+        int buttonX = 850;
+        int buttonY = 398;
+
+        boton.setBounds(buttonX, buttonY, 200, 30); // Establecer posición y tamaño
+        panel1.add(boton);
+        add(panel1); // Agregar al contenedor principal
+
+        setVisible(true);
+
+        boton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                // Cerrar ventana
+                dispose();
+
+                // Abrir nueva ventana
+                Asientos nuevoasiento = new Asientos();
+                nuevoasiento.setVisible(true);
+            }
+        });
     }
 }
 
+class Asientos extends  JFrame{
 
+    Image imagenfondo1;
+    public Asientos() {
+        setTitle("Asientos");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        int screenWidth = screenSize.width;
+        int screenHeight = screenSize.height;
+
+        // Configurar el tamaño de la ventana para ocupar toda la pantalla
+        setSize(screenWidth, screenHeight);
+
+        imagenfondo1 = new ImageIcon("C:/Users/user/Downloads/PROGRA2/progra2proyecto/src/dibujos/autobus3.png").getImage();
+        imagenfondo1 = imagenfondo1.getScaledInstance(screenWidth, screenHeight, Image.SCALE_SMOOTH);
+        JLabel principal = new JLabel(new ImageIcon(imagenfondo1));
+        setContentPane(principal);
+
+        JLabel label = new JLabel("Asientos");
+        label.setBounds(400, 165, 200, 20); // Establecer la posición y el tamaño del JLabel
+        label.setForeground(new Color(35, 35, 35)); // Color de fuente más oscuro
+        Font font = label.getFont();
+        Font fontNueva = font.deriveFont(font.getSize() + 4.0f); // Aumentar en 2 puntos el tamaño de la fuente
+        label.setFont(fontNueva);
+        add(label);
+
+
+    }
+}
