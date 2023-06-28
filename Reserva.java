3097bb7d438a3c940f360bc230374149e04572fd
natibelen <131008881+natibelen@users.noreply.github.com>
2023-06-28 19:41:49 -0400

    Add files via upload
    se empieza a trabajar en la pestaña que confirma los numeros de asientos

--- Reserva.java
+++ Reserva.java
@@ -2,48 +2,14 @@ package org.example;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Reserva extends JFrame {
 
     Image imagenfondo1;
     private JPanel panel1;
     String desde,hasta,dia;
-    private JButton botonReserva;
-
-    private void mostrarDatos(List<DatosViaje> listaDatos) {
-
-        int[] x = {300,550,300,550};
-        int[] y = {300, 300, 330,330};
-        int buttonX = 900;
-        int buttonY = 315;
-
-        JLabel[] labels = new JLabel[4];
-
-        for (DatosViaje datosViaje : listaDatos) {
-            String asientosSeleccionados=datosViaje.getDestino();
-
-            labels[0]= new JLabel("Asientos reservados: " + asientosSeleccionados);
-
-            for (int j = 0; j < 4; j++) {
-                labels[j].setForeground(new Color(35, 35, 35)); // Color de fuente
-                Font font = labels[j].getFont();
-                Font fontNueva = font.deriveFont(font.getSize() + 2.0f); // Tamaño de la fuente
-                labels[j].setFont(fontNueva);
-                labels[j].setBounds(x[j], y[j], 600, 30);
-                add(labels[j]);
-            }
-
-            for(int i=0;i<4;i++){
-                y[i]+=90;
-            }
-
-            buttonY += 90;
-        }
-    }
+    private JButton botonExit;
 
     public Reserva() {
 
@@ -64,9 +30,14 @@ public class Reserva extends JFrame {
         panel1 = new JPanel();
         panel1.setBounds(850, 398, 200, 30);
         panel1.setOpaque(false);
+
+        botonExit = new JButton("Reservar");
+        botonExit.setFont(botonExit.getFont().deriveFont(Font.PLAIN)); // Cambiar el estilo de fuente del botón
+        botonExit.setBounds(1100, 300, 100, 30);
+        add(botonExit);
     }
 
-    public void agregaasieentos(Asientos asientos){
+    public void agregaasieentos(AsientosDob asientos){
         ArrayList seleccionAsientos = asientos.getSeleccion();
 
         JLabel asientosSelecc = new JLabel(seleccionAsientos.toString());
