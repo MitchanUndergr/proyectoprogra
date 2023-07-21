5d46ffdebf25418247ea2ae2b96c3e513bf104d1
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-07-21 01:50:59 -0400

    se comenta AsientoGUIFactory.java

--- AsientoGUIFactory.java
+++ AsientoGUIFactory.java
@@ -1,43 +1,45 @@
 public class AsientoGUIFactory {
+    // Atributos de la clase
+    private Asientos asiento = new Asientos();
+    private static final String SEMI = "Semi cama";
+    private boolean semicama = false;
+    private boolean saloncama = false;
+    private AsientosGUI Semic, Salonc;
+    private static final String SALON = "Salon cama";
 
-    private Asientos asiento=new Asientos();
-    private static final String Semi= "Semi cama";
-    private boolean semicama=false;
-    private boolean saloncama=false;
-    private AsientosGUI Semic,Salonc;
-    private static final String Salon= "Salon cama";
-    public AsientosGUI crearAsientoGUI(String tipo){
-        if (tipo.equalsIgnoreCase(Semi)) {
-            semicama=true;
-            Semic= new AsientosSemiGUI();
+    // Método para crear una instancia de la clase AsientosGUI según el tipo de asiento
+    public AsientosGUI crearAsientoGUI(String tipo) {
+        if (tipo.equalsIgnoreCase(SEMI)) {
+            semicama = true;
+            Semic = new AsientosSemiGUI();
             return Semic;
-        } else if (tipo.equalsIgnoreCase(Salon)) {
-            saloncama=true;
-            Salonc= new AsientosSalonGUI();
+        } else if (tipo.equalsIgnoreCase(SALON)) {
+            saloncama = true;
+            Salonc = new AsientosSalonGUI();
             return Salonc;
         } else {
-            return null; // En caso de que el tipo no sea válido
+            return null; // En caso de que el tipo no sea válido, retorna null
         }
     }
-    public void UsarAsientoGUI(){
-        if(semicama){
-            Semic.ElegirAsientos();
-            Semic.ImplementarFiltro();
-            if(asiento.getSeleccionAsientos().size()<40) {
-                Semic.setVisible(true);
-            }
-            else{
-                Semic.setVisible(false);
+
+    // Método para utilizar la interfaz de asientos según el tipo de asiento
+    public void UsarAsientoGUI() {
+        if (semicama) {
+            Semic.ElegirAsientos(); // Muestra la interfaz para elegir asientos (semi cama)
+            Semic.ImplementarFiltro(); // Implementa el filtro para mostrar los datos
+            if (asiento.getSeleccionAsientos().size() < 40) {
+                Semic.setVisible(true); // Si elige menos de 40 asientos, muestra la interfaz
+            } else {
+                Semic.setVisible(false); // Si elige 40 asientos o más, oculta y cierra la interfaz
                 Semic.dispose();
             }
         } else if (saloncama) {
-            Salonc.ElegirAsientos();
-            Salonc.ImplementarFiltro();
-            if(asiento.getSeleccionAsientosdob().size()<60){
-                Salonc.setVisible(true);
-            }
-            else{
-                Salonc.setVisible(false);
+            Salonc.ElegirAsientos(); // Muestra la interfaz para elegir asientos (salón cama)
+            Salonc.ImplementarFiltro(); // Implementa el filtro para mostrar los datos
+            if (asiento.getSeleccionAsientosdob().size() < 60) {
+                Salonc.setVisible(true); // Si elige menos de 60 asientos, muestra la interfaz
+            } else {
+                Salonc.setVisible(false); // Si elige 60 asientos o más, oculta y cierra la interfaz
                 Salonc.dispose();
             }
         }
