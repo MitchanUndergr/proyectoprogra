7adf9dfd7b681d192c1474f51bef94077e8db616
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-07-21 00:39:15 -0400

    ahora es una clase logica
    funciona en conjunto con la nueva clase ReservaGUI

--- Reserva.java
+++ Reserva.java
@@ -1,260 +1,69 @@
-import javax.swing.*;
-import java.awt.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Reserva {
 
-public class Reserva extends JFrame {
+    private String Origen,Destino,fechas,Hora,Tipoasiento;
+    int precio;
+    public Reserva(){
+        try {
+            FileReader fileReader = new FileReader("src/Archivos/DatosPasaje.txt");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String linea;
 
-    Image imagenfondo1;
-    private JPanel panel1;
-    public ArrayList<String> arrayasientodob,arrayasientos;
-    String desde,hasta,dia;
-    public ArrayList<Object[]> datosdobreserva,datosreserva;
-    public Reserva() {
+            // Variables para almacenar los datos de la última línea
+            String ultimoOrigen = null;
+            String ultimoDestino = null;
+            String ultimaFecha = null;
+            String ultimaHora = null;
+            String ultimoTasiento = null;
+            int ultimoprecio = 0;
 
-        setTitle("Reserva");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            while ((linea = bufferedReader.readLine()) != null) {
+                // Separar los datos utilizando el carácter de coma como delimitador
+                String[] datos = linea.split(",");
 
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        int screenWidth = screenSize.width;
-        int screenHeight = screenSize.height;
+                // Almacenar los datos en variables temporales
+                ultimoOrigen = datos[0].trim();
+                ultimoDestino = datos[1].trim();
+                ultimaFecha = datos[2].trim();
+                ultimaHora = datos[3].trim();
+                ultimoTasiento = datos[4].trim();
+                ultimoprecio = Integer.parseInt(datos[5]);
 
-        setSize(screenWidth, screenHeight);
-
-        imagenfondo1 = new ImageIcon("C:/Users/user/Downloads/PROGRA2/progra2proyecto/src/dibujos/compra.png").getImage();
-        imagenfondo1 = imagenfondo1.getScaledInstance(screenWidth, screenHeight, Image.SCALE_SMOOTH);
-        JLabel principal = new JLabel(new ImageIcon(imagenfondo1));
-        setContentPane(principal);
-
-        panel1 = new JPanel();
-        panel1.setBounds(850, 398, 200, 30);
-        panel1.setOpaque(false);
-
-
-    }
-
-   public ArrayList<String> agregaasieentosDob(AsientosDob asientos){
-        ArrayList asie=asientos.getSelecciondob();
-
-        return asie;
-    }
-
-    public ArrayList<String> agregaasieentos(Asientos asientos){
-        ArrayList asie=asientos.getSeleccion();
-        return  asie;
-    }
-
-    public ArrayList<String> getArrayasientodob(){
-        return arrayasientodob;
-    }
-
-    public ArrayList<String> getArrayasientos(){
-        return arrayasientos;
-    }
-
-
-    public void agregafiltro(Asientos asientos){
-        dia=asientos.getDia();
-        desde=asientos.getDesde();
-        hasta=asientos.getHasta();
-
-        JLabel origen1 = new JLabel(desde);
-        origen1.setForeground(new Color(35, 35, 35)); // Color de fuente
-        Font font = origen1.getFont();
-        Font fontNueva = font.deriveFont(font.getSize() + 2.0f); // Tamaño de la fuente
-        origen1.setFont(fontNueva);
-        origen1.setBounds(95, 110, 100, 30);
-        add(origen1);
-
-        JLabel destino = new JLabel(hasta);
-        destino.setForeground(new Color(35, 35, 35)); // Color de fuente
-        Font font2 = destino.getFont();
-        Font fontNueva2 = font2.deriveFont(font.getSize() + 2.0f); // Tamaño de la fuente
-        destino.setFont(fontNueva2);
-        destino.setBounds(285, 110, 100, 30);
-        add(destino);
-
-        JLabel fecha = new JLabel(dia);
-        fecha.setForeground(new Color(35, 35, 35)); // Color de fuente
-        Font font3 = fecha.getFont();
-        Font fontNueva3 = font3.deriveFont(font.getSize() + 2.0f); // Tamaño de la fuente
-        fecha.setFont(fontNueva3);
-        fecha.setBounds(470, 110, 100, 30);
-        add(fecha);
-
-        JLabel[] labels = new JLabel[6];
-
-        int calFinal= asientos.getSeleAsientos().size()*13000;
-        int cantAsientos=asientos.getSeleAsientos().size();
-
-        labels[1]= new JLabel("DETALLES DE LA COMPRA");
-        labels[1].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[1].setFont(fontNueva);
-        labels[1].setBounds(300, 350, 600, 30);
-        add(labels[1]);
-
-        labels[2]= new JLabel("Tipo de Asiento: " + "Semi Cama");
-        labels[2].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[2].setFont(fontNueva);
-        labels[2].setBounds(300, 400, 600, 30);
-        add(labels[2]);
-
-        labels[3]= new JLabel("Asientos : " + asientos.getSeleAsientos());
-        labels[3].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[3].setFont(fontNueva);
-        labels[3].setBounds(300, 450, 600, 30);
-        add(labels[3]);
-
-        labels[4]= new JLabel("Cantidad de asientos: " + cantAsientos + " x " + "13000");
-        labels[4].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[4].setFont(fontNueva);
-        labels[4].setBounds(300, 500, 600, 30);
-        add(labels[4]);
-
-        labels[5]= new JLabel("Precio final: " + calFinal);
-        labels[5].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[5].setFont(fontNueva);
-        labels[5].setBounds(300, 550, 600, 30);
-        add(labels[5]);
-
-        SwingUtilities.invokeLater(() -> {
-
-            int option = JOptionPane.showOptionDialog(
-                    null,
-                    "¿Desea realizar una nueva compra?",
-                    "Confirmación",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    new Object[]{"Sí", "No"},
-                    "Sí"
-            );
-
-            if (option == JOptionPane.YES_OPTION) {
-                // Abrir una nueva interfaz gráfica
-
-
-                EventQueue.invokeLater(() -> {
-                    InterfazGrafica nuevaInterfaz = new InterfazGrafica();
-                    ArrayList<String> asien=nuevaInterfaz.arrayAsientos(this);
-                    nuevaInterfaz.asiento=asien;
-                    nuevaInterfaz.getAsiento();
-                    //System.out.println(nuevaInterfaz.asiento.get(0));
-                    nuevaInterfaz.traslado();
-                    //System.out.println("HOLI");
-                    nuevaInterfaz.setVisible(true);
-                    dispose();
-                });
-            } else {
-                // Cerrar la aplicación
-                System.exit(0);
             }
 
+            // Actualizar las variables de la instancia con los datos de la última línea
+            this.Origen = ultimoOrigen;
+            this.Destino = ultimoDestino;
+            this.fechas = ultimaFecha;
+            this.Hora = ultimaHora;
+            this.Tipoasiento= ultimoTasiento;
+            this.precio= ultimoprecio;
 
-        });
-
-
+            bufferedReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    public void agregafiltroDob(AsientosDob asientos){
-        dia=asientos.getDia();
-        desde=asientos.getDesde();
-        hasta=asientos.getHasta();
-
-        JLabel origen1 = new JLabel(desde);
-        origen1.setForeground(new Color(35, 35, 35)); // Color de fuente
-        Font font = origen1.getFont();
-        Font fontNueva = font.deriveFont(font.getSize() + 2.0f); // Tamaño de la fuente
-        origen1.setFont(fontNueva);
-        origen1.setBounds(95, 110, 100, 30);
-        add(origen1);
-
-        JLabel destino = new JLabel(hasta);
-        destino.setForeground(new Color(35, 35, 35)); // Color de fuente
-        Font font2 = destino.getFont();
-        Font fontNueva2 = font2.deriveFont(font.getSize() + 2.0f); // Tamaño de la fuente
-        destino.setFont(fontNueva2);
-        destino.setBounds(285, 110, 100, 30);
-        add(destino);
-
-        JLabel fecha = new JLabel(dia);
-        fecha.setForeground(new Color(35, 35, 35)); // Color de fuente
-        Font font3 = fecha.getFont();
-        Font fontNueva3 = font3.deriveFont(font.getSize() + 2.0f); // Tamaño de la fuente
-        fecha.setFont(fontNueva3);
-        fecha.setBounds(470, 110, 100, 30);
-        add(fecha);
-
-        JLabel[] labels = new JLabel[6];
-
-        int calFinal= asientos.getSeleasientosdob().size()*21000;
-        int cantAsientos=asientos.getSeleasientosdob().size();
-
-
-        labels[1]= new JLabel("DETALLES DE LA COMPRA");
-        labels[1].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[1].setFont(fontNueva);
-        labels[1].setBounds(300, 350, 600, 30);
-        add(labels[1]);
-
-        labels[2]= new JLabel("Tipo de Asiento: " + "Salon Cama");
-        labels[2].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[2].setFont(fontNueva);
-        labels[2].setBounds(300, 400, 600, 30);
-        add(labels[2]);
-
-        labels[3]= new JLabel("Asientos : " + asientos.getSeleasientosdob());
-        labels[3].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[3].setFont(fontNueva);
-        labels[3].setBounds(300, 450, 600, 30);
-        add(labels[3]);
-
-        labels[4]= new JLabel("Cantidad de asientos: " + cantAsientos + " x " + "21000");
-        labels[4].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[4].setFont(fontNueva);
-        labels[4].setBounds(300, 500, 600, 30);
-        add(labels[4]);
-
-        labels[5]= new JLabel("Precio final: " + calFinal);
-        labels[5].setForeground(new Color(35, 35, 35)); // Color de fuente
-        labels[5].setFont(fontNueva);
-        labels[5].setBounds(300, 550, 600, 30);
-        add(labels[5]);
-
-        SwingUtilities.invokeLater(() -> {
-
-            int option = JOptionPane.showOptionDialog(
-                    null,
-                    "¿Desea realizar una nueva compra?",
-                    "Confirmación",
-                    JOptionPane.YES_NO_OPTION,
-                    JOptionPane.QUESTION_MESSAGE,
-                    null,
-                    new Object[]{"Sí", "No"},
-                    "Sí"
-            );
-
-            if (option == JOptionPane.YES_OPTION) {
-                // Abrir una nueva interfaz gráfica
-
-                EventQueue.invokeLater(() -> {
-                    InterfazGrafica nuevaInterfaz = new InterfazGrafica();
-                    ArrayList<String> asien2= nuevaInterfaz.arrayAsientosDob(this);
-                    nuevaInterfaz.datadob=datosdobreserva;
-                    nuevaInterfaz.getDatadob();
-                    nuevaInterfaz.asientoDob=asien2;
-                    nuevaInterfaz.getAsientoDob();
-                    nuevaInterfaz.trasladodob();
-                    nuevaInterfaz.setVisible(true);
-                    dispose();
-                });
-            } else {
-                // Cerrar la aplicación
-                System.exit(0);
-            }
-
-        });
-
+    public String getOrigen(){
+        return Origen;
     }
-
+    public String getDestino(){
+        return Destino;
+    }
+    public String getFechas(){
+        return fechas;
+    }
+    public String getHora(){
+        return Hora;
+    }
+    public String getTipoasiento(){
+        return Tipoasiento;
+    }
+    public int getPrecio(){
+        return precio;
+    }
 }
