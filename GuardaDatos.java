e6be6142abab8c070ec4614fcd51225082d5c6df
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-07-21 01:47:18 -0400

    se comenta GuardaDatos.java

--- GuardaDatos.java
+++ GuardaDatos.java
@@ -4,15 +4,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class GuardaDatos {
-
+    // Rutas de los archivos
     private static final String PRIMER_ARCHIVO = "src/Archivos/filtro_pasajes.txt";
     private static final String SEGUNDO_ARCHIVO = "src/Archivos/filtro_asientos.txt";
     private static final String TERCER_ARCHIVO = "src/Archivos/DatosPasaje.txt";
+
+    // Variables para verificar si los archivos fueron creados correctamente
     private static boolean archivoCreado = false;
     private static boolean archivoCreado2 = false;
     private static boolean archivoCreado3 = false;
 
-    // Método para crear el archivo (solo se debe llamar una vez)
+    // Método para crear el primer archivo (filtro_pasajes.txt)
     public static void crearArchivo() {
         try {
             File file = new File(PRIMER_ARCHIVO);
@@ -35,7 +37,7 @@ public class GuardaDatos {
         }
     }
 
-
+    // Método para crear el segundo archivo (filtro_asientos.txt)
     public static void crearArchivo2() {
         try {
             File file = new File(SEGUNDO_ARCHIVO);
@@ -58,6 +60,7 @@ public class GuardaDatos {
         }
     }
 
+    // Método para crear el tercer archivo (DatosPasaje.txt)
     public static void crearArchivo3() {
         try {
             File file = new File(TERCER_ARCHIVO);
@@ -80,7 +83,7 @@ public class GuardaDatos {
         }
     }
 
-    // Método para escribir en el primer archivo
+    // Método para escribir en el primer archivo (filtro_pasajes.txt)
     public static void guardarDatosU(String origen, String destino, String fecha) {
         try {
             FileWriter writer = new FileWriter(PRIMER_ARCHIVO, true); // true para escribir al final del archivo
@@ -93,7 +96,7 @@ public class GuardaDatos {
         }
     }
 
-    // Método para escribir en el segundo archivo
+    // Método para escribir en el segundo archivo (filtro_asientos.txt)
     public static void guardarDatosD(String origen, String destino, String fecha, String Hora, String Tasiento, int precio, ArrayList<String> datos) {
         try {
             FileWriter writer = new FileWriter(SEGUNDO_ARCHIVO, true); // true para añadir al final del archivo
@@ -104,13 +107,12 @@ public class GuardaDatos {
             writer.write(Tasiento + ",");
             writer.write(precio + ",");
 
-            // Recorrer los datos
+            // Recorrer los datos y escribirlos en el archivo
             for (int i = 0; i < datos.size(); i++) {
                 String dato = datos.get(i);
                 writer.write(dato);
 
-                // Si no es el último dato, escribir un espacio
-                // Si es el último dato, escribir un salto de línea
+                // Si no es el último dato, escribir un espacio, de lo contrario, escribir un salto de línea
                 if (i < datos.size() - 1) {
                     writer.write(" ");
                 } else {
@@ -124,6 +126,7 @@ public class GuardaDatos {
         }
     }
 
+    // Método para escribir en el tercer archivo (DatosPasaje.txt)
     public static void guardarDatosT(String origen, String destino, String fecha, String horario, String Tasiento, int precio) {
         try {
             FileWriter writer = new FileWriter(TERCER_ARCHIVO, true); // true para añadir al final del archivo
