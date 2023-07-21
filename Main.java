3c1b9bdd46d75859aa60a6eb44d2fb25afbda9d0
MitchanUndergr <131836786+MitchanUndergr@users.noreply.github.com>
2023-07-21 00:28:31 -0400

    Main llama a nuevos metodos
    ahora main llama a nuevos metodos creados en interfaz grafica y llama al metodo de la nueva clase GuardaDatos

--- Main.java
+++ Main.java
@@ -1,7 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
-        InterfazGrafica interfaz = new InterfazGrafica();
-        interfaz.traslado();
 
+public class Main {
+    public static void main(String[] args)  {
+        InterfazGrafica interfaz = new InterfazGrafica();
+        interfaz.setVisible(true);
+        interfaz.BarrasSeleccion();
+        GuardaDatos.crearArchivo();
+        GuardaDatos.crearArchivo2();
+        GuardaDatos.crearArchivo3();
     }
 }
